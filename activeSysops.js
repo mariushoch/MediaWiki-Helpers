a90913a253b389a32f4348a697084f82cce9fd84
Marius Hoch <hoo@online.de>
2013-05-24 14:29:06 +0200

    Cache API data locally in activeSysops

--- activeSysops.js
+++ activeSysops.js
@@ -1,5 +1,6 @@
 /*
-* [[m:user:Hoo man]]; Version 4.0; 2013-01-13;
+* [[m:user:Hoo man]]; Version 5.0; 2013-05-24;
+*
 * Shows the number of active (one log entry in the last 7 days or as configured) sysops, uses data from the toolserver
 * Tested in IE and FF with vector and monobook, uses my (Hoo man) wiki tools (shared.js)
 *
@@ -9,8 +10,8 @@
 /*global hoo, mw, activeSysopsConfig, disable_activeSysops */
 /*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, loopfunc:true, bitwise:true, undef:true, browser:true, jquery:true, indent:4, maxerr:50, white:false */
 
-mw.loader.using( [ 'mediawiki.util', 'mediawiki.jqueryMsg' ], function() {
-	"use strict";
+mw.loader.using( [ 'mediawiki.util', 'mediawiki.jqueryMsg', 'jquery.jStorage' ], function() {
+	'use strict';
 
 	// Default config
 	var config = {
@@ -20,39 +21,132 @@ mw.loader.using( [ 'mediawiki.util', 'mediawiki.jqueryMsg' ], function() {
 			markWikisActiveSysopsLessThan : 2,
 			markWikisGS : false,
 			// Sorting order
-			markWikisActiveSysopsFirst : false,
-			markWikisGSFirst : true
+			markWikisActiveSysopsFirst : false
 		},
 		$wikiMarker, $activeSysopMarker, $GSWikiMarker;
 
-	function onloadSysopCount( data ) {
-		if ( data.api.error !== 'false' ) {
-			return;
-		}
-		var msg = mw.message( 'hoo-activeSysops-toolbarText', data.api.activesysops.count ).escaped();
-		hoo.addToolLink( msg, '//toolserver.org/~pathoschild/stewardry/?wiki=' + mw.config.get( 'wgDBname' ) + '_p&sysop=on&bureaucrat=on', '', config.toolLinkMethod );
-		if ( config.markWikisActiveSysops && data.api.activesysops.count < config.markWikisActiveSysopsLessThan ) {
+	/**
+	 * Display the number of sysops
+	 *
+	 * @param {number} count
+	 */
+	function displaySysopCount( count ) {
+		var msg = mw.message( 'hoo-activeSysops-toolbarText', count ).escaped();
+
+		hoo.addToolLink(
+			msg,
+			'//toolserver.org/~pathoschild/stewardry/?wiki=' + mw.config.get( 'wgDBname' ) + '_p&sysop=on&bureaucrat=on',
+			'',
+			config.toolLinkMethod
+		);
+
+		if ( config.markWikisActiveSysops && count < config.markWikisActiveSysopsLessThan ) {
 			$activeSysopMarker = $( '<span>' )
 				.attr( 'id', 'activeSysopMarker' )
-				.html( msg )
-				.appendTo( $wikiMarker );
+				.text( msg )
+				.appendTo( $wikiMarker )
+				// Button sorting (which one is left most), only needed if both buttons are present
+				.css( 'float', config.markWikisActiveSysopsFirst ? 'left' : 'right' );
 		}
 	}
 
-	function onloadWikiSet( data ) {
-		if ( data.api.error !== 'false' || !data.api.wikisets[0].ws_wikis ) {
-			return false;
+	/**
+	 * Display that the wiki is a GS wiki... if it is
+	 *
+	 * @param {bool} isGSWiki
+	 */
+	function displayGSWikiSet( isGSWiki ) {
+		if ( !isGSWiki ) {
+			return;
 		}
-		if( $.inArray( mw.config.get( 'wgDBname' ), data.api.wikisets[0].ws_wikis ) !== -1 ) {
-			return false;
-		}
+
 		$GSWikiMarker = $( '<span>' )
 			.attr( 'id', 'GSWikiMarker' )
-			.html( mw.message( 'hoo-activeSysops-GSWiki' ).escaped() )
-			.appendTo( $wikiMarker );
+			.text( mw.message( 'hoo-activeSysops-GSWiki' ).escaped() )
+			.appendTo( $wikiMarker )
+			// Button sorting (which one is left most), only needed if both buttons are present
+			.css( 'float', config.markWikisActiveSysopsFirst ? 'right' : 'left' );
 	}
 
-	// Init code
+	/**
+	 * Get the number of sysops either from cache or the TS API and call callback with it
+	 *
+	 * @param {function} callback
+	 */
+	function getSysopCount( callback ) {
+		var storageKey = 'hoo-activeSysops-sysopCount';
+
+		if ( $.jStorage.get( storageKey, false ) ) {
+			callback( $.jStorage.get( storageKey ) );
+			return;
+		}
+
+		$.ajax( {
+			url: '//toolserver.org/~hoo/api.php',
+			data: {
+				action: 'activeSysops',
+				wiki: mw.config.get( 'wgDBname' ) + '_p',
+				format: 'json',
+				last_action: config.lastAction
+			},
+			dataType: 'jsonp'
+		} )
+		.done( function( data ) {
+			if ( data.api.error !== 'false' ) {
+				return;
+			}
+			$.jStorage.set( storageKey, data.api.activesysops.count );
+			// Expire after 25h
+			$.jStorage.setTTL( storageKey, 25 * 3600 * 1000 );
+
+			callback( $.jStorage.get( storageKey ) );
+		} );
+	}
+
+	/**
+	 * Get the global sysop wikiset either from cache or the TS API and call callback with it
+	 *
+	 * @param {function} callback
+	 */
+	function getGSWikiSet( callback ) {
+		var storageKey = 'hoo-activeSysops-GSWikiSet';
+
+		if ( $.jStorage.get( storageKey, false ) ) {
+			callback( $.jStorage.get( storageKey ) );
+			return;
+		}
+
+		$.ajax( {
+			url: '//toolserver.org/~hoo/api.php',
+			data: {
+				action: 'wikiSets',
+				wikiset: 7,
+				format: 'json',
+				prop: 'ws_wikis'
+			},
+			dataType: 'jsonp'
+		} )
+		.done( function( data ) {
+			var isGSWiki = false;
+
+			if ( data.api.error !== 'false' || !data.api.wikisets[0].ws_wikis ) {
+				return false;
+			}
+			if( $.inArray( mw.config.get( 'wgDBname' ), data.api.wikisets[0].ws_wikis ) === -1 ) {
+				isGSWiki = true;
+			}
+
+			$.jStorage.set( storageKey, isGSWiki );
+			// Expire after 25h
+			$.jStorage.setTTL( storageKey, 25 * 3600 * 1000 );
+
+			callback( $.jStorage.get( storageKey ) );
+		} );
+	}
+
+	/**
+	 * Init code
+	 */
 	function init() {
 		var lang;
 
@@ -75,13 +169,13 @@ mw.loader.using( [ 'mediawiki.util', 'mediawiki.jqueryMsg' ], function() {
 
 		if ( lang === 'de' ) {
 			mw.messages.set( {
-				'hoo-activeSysops-toolbarText' : '$1 aktive{{PLURAL:$1|r|}} Administrator{{PLURAL:$1||en}}',
-				'hoo-activeSysops-GSWiki' : 'Globales Administratoren wiki'
+				'hoo-activeSysops-toolbarText': '$1 aktive{{PLURAL:$1|r|}} Administrator{{PLURAL:$1||en}}',
+				'hoo-activeSysops-GSWiki': 'Globales Administratoren wiki'
 			} );
 		} else {
 			mw.messages.set( {
-				'hoo-activeSysops-toolbarText' : '$1 active sysop{{PLURAL:$1||s}}',
-				'hoo-activeSysops-GSWiki' : 'Global sysop wiki'
+				'hoo-activeSysops-toolbarText': '$1 active sysop{{PLURAL:$1||s}}',
+				'hoo-activeSysops-GSWiki': 'Global sysop wiki'
 			} );
 		}
 
@@ -90,45 +184,14 @@ mw.loader.using( [ 'mediawiki.util', 'mediawiki.jqueryMsg' ], function() {
 			$wikiMarker = $( '<div>' )
 				.attr( 'id', 'wikiMarker' )
 				.appendTo( 'body' );
-
-			// Button sorting (which one is left most), only needed if both buttons are present
-			if ( config.markWikisActiveSysopsFirst ) {
-				mw.util.addCSS( '#activeSysopMarker { float: left; } #GSWikiMarker { float: right; }' );
-			} else {
-				mw.util.addCSS( '#activeSysopMarker { float: right; } #GSWikiMarker { float: left; }' );
-			}
 		}
 
-		// Load the active sysop count from Toolserver
-		$.ajax( {
-			url: '//toolserver.org/~hoo/api.php',
-			data: {
-				action: 'activeSysops',
-				wiki: mw.config.get( 'wgDBname' ) + '_p',
-				format: 'json',
-				last_action: config.lastAction
-			},
-			cache: true,
-			dataType: 'jsonp',
-			jsonpCallback: 'onloadSysopCount'
-		} )
-		.done( onloadSysopCount );
+		// Load the active sysop count
+		getSysopCount( displaySysopCount );
 
 		if( config.markWikisGS ) {
-			// Load the wiki set information from Toolserver
-			$.ajax( {
-				url: '//toolserver.org/~hoo/api.php',
-				data: {
-					action: 'wikiSets',
-					wikiset: 7,
-					format: 'json',
-					prop: 'ws_wikis'
-				},
-				cache: true,
-				dataType: 'jsonp',
-				jsonpCallback: 'onloadWikiSet'
-			} )
-			.done( onloadWikiSet );
+			// Load the wiki set information
+			getGSWikiSet( displayGSWikiSet );
 		}
 	}
 
