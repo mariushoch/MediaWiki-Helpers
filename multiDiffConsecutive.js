4f6441c45ae4fd616f965579b151d9280e0d27bb
Marius Hoch <hoo@online.de>
2013-06-18 00:13:10 +0200

    Add license to multiDiffConsecutive.js

--- multiDiffConsecutive.js
+++ multiDiffConsecutive.js
@@ -2,6 +2,21 @@
  * View the next/ previous edit of a revision in MediaWiki multi diffs.
  * Uses the localization provided by MediaWiki, requires mediawiki.util to work.
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
  * @author Marius Hoch < hoo@online.de >
  */
 ( function( mw, $ ) {
